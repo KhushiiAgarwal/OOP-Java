30c277dcf6fb915a1ca1c1d920e6aced0ae4ae3f
Khushi A <99123453+KhushiiAgarwal@users.noreply.github.com>
2024-01-13 23:46:53 +0530

    Recursive method
    Co-authored by: @UdayJaju24 <udayjaju24@gmail.com>
    Co-authored by: @KhushiiAgarwal <khushi.agawal21@vit.edu>

--- Factorial.java
+++ Factorial.java
@@ -1,12 +1,15 @@
 import java.util.*;  
 class Factorial{
     public static int Factorial(int num){
-        if(num==0 ||num==1){
-            return 1;
+        if(num>0){
+            if(num==0 ||num==1){
+                return 1;
+            }
         }
         int fact=0;
         return num*Factorial(num-1);
     }
+    
     public static void main(String[] args){
         int num,fac=0,i=1;
         Scanner sc=new Scanner(System.in);
